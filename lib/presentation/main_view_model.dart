84d8b01597cc68dbcfcd8d17df257995f32ffe37
kimkangtae <tt997768@gmail.com>
2024-04-07 04:53:19 +0900

    Todo

--- lib/presentation/main_view_model.dart
+++ lib/presentation/main_view_model.dart
@@ -4,13 +4,9 @@ import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:naver_map_test/data/data_source/my_locate.dart';
 
 class MainViewModel extends ChangeNotifier {
-  // final NaverMapController _mapController;
   double longitude = 36.8431728;
   double latitude = 127.173850;
 
-  // MainViewModel(this._mapController);
-
-  // 기존 naver_map_plugin에서 사용하는 LocationService는 사용불가
   // GeoLocator로 구현
   Future<void> getLocationData() async {
     MyLocation myLocation = MyLocation();
@@ -22,4 +18,8 @@ class MainViewModel extends ChangeNotifier {
     );
     notifyListeners();
   }
+
+  //TODO Create Marker Set<> MockData
+
+  //TODO How to Show 500m Information
 }
